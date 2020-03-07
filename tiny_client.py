82a0fd034799212484c5a6acff12b7dcaebea857
Sean C. Fontellio <20scf6@sharabi.cs.williams.edu>
2020-03-07 16:25:42 -0500

    client python edit

--- tiny_client.py
+++ tiny_client.py
@@ -5,6 +5,22 @@ name = "http://"+sys.argv[1]+":8888"
 
 server = xmlrpclib.Server(name)
 
-print server.service.lookup("53477")
-print server.service.buy("53477")
-print server.service.lookup("53477")
+while True:
+
+	request = sys.stdin.readline()
+
+	cmd = request.strip().split(' ')
+	print cmd
+
+	if cmd[0] == "lookup":
+		print server.service.lookup(cmd[1])
+	elif cmd[0] == "buy":
+		print server.service.buy(cmd[1])
+	elif cmd[0] == "search":
+		print server.service.search(cmd[1])
+	elif cmd[0] == "exit":
+		break
+	else:
+		print "usage: lookup item number | buy item number | search topic | exit"
+
+
